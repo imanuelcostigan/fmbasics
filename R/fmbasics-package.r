dbb8f254c19b45bf7483c9e67ff8cdaec057c70f
Imanuel Costigan <i.costigan@me.com>
2017-01-15 13:13:45 +1100

    Added package documentation

---
+++ R/fmbasics-package.r
@@ -0,0 +1,12 @@
+#' fmbasics: Financial Market Building Blocks
+#'
+#' Implements basic financial market objects like currencies, currency pairs,
+#' interest rates and interest rate indices. You will be able to use Benchmark
+#' instances of these objects which have been defined using their most common
+#' conventions or those defined by International Swap Dealer Association (ISDA,
+#' http://www2.isda.org) legal documentation.
+#'
+#' @name fmbasics
+#' @docType package
+#' @import fmdates
+NULL
