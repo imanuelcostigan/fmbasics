90e6f4ec6f3a8b203e411949c99c3466a4c33699
Imanuel Costigan <i.costigan@me.com>
2017-01-15 13:13:30 +1100

    Translated Currency class and handy constructors

--- R/currency-class.R
+++ R/currency-class.R
@@ -1,83 +1,93 @@
-#' @include date-methods.R
-NULL
-
-#' Class \code{"Currency"}
+#' Build a Currency
 #'
 #' A currency refers to money in any form when in actual use or circulation, as
-#' a medium of exchange, especially circulating paper money.
+#' a medium of exchange, especially circulating paper money. This package
+#' includes handy constructors for common currencies.
 #'
-#' @section Methods:
-#' \describe{
-#' \item{initialize(iso, calendar)}{Creates a Currency object with \code{iso}
-#' (a string) and \code{calendar} (inherits from \code{\link{JointCalendar}})}
-#' \item{as.character()}{A string representation}
-#' \item{print()}{Prints the string representation to the console}
+#' @section Handy constructors:
+#'
+#' The handy currency constructors use the following conventions:
+#'
+#' \tabular{ll}{
+#' \bold{Creator} \tab \bold{Joint calendars} \cr
+#' `AUD()` \tab `AUSYCalendar` \cr
+#' `EUR()` \tab `EUTACalendar` \cr
+#' `GBP()` \tab `GBLOCalendar` \cr
+#' `JPY()` \tab `JPTOCalendar` \cr
+#' `NZD()` \tab `NZAUCalendar`, `NZWECalendar` \cr
+#' `USD()` \tab `USNYCalendar` \cr
+#' `CHF()` \tab `CHZHCalendar` \cr
+#' `HKD()` \tab `HKHKCalendar` \cr
+#' `NOK()` \tab `NOOSCalendar`
 #' }
-#' @section Active binds:
-#' \code{Currency} has two active bindings: \code{iso} and \code{calendar}.
-#' The former returns/sets the currency's ISO string while the latter returns/sets
-#' the currency's financial centre (as a \code{\link{JointCalendar}}).
+#'
+#' @param iso a three letter code representing the currency (see [ISO
+#'   4217](https://en.wikipedia.org/wiki/ISO_4217))
+#' @param calendar a [JointCalendar][fmdates::JointCalendar()]
+#' @return an object of class `Currency`
 #'
 #' @references
 #' \href{http://en.wikipedia.org/w/index.php?title=Currency&oldid=598027200}{Currency. (2014, March 3). In Wikipedia}
-#' \href{http://en.wikipedia.org/wiki/ISO_currency_code}{ISO 4217. (2014, February 25). In Wikipedia}
-#'
 #' @examples
-#' AUD_USER <- Currency$new("AUD", c(AUSYCalendar$new()))
-#'
-#' @docType class
-#' @format A \code{\link{R6Class}} generator object
-#' @keywords data
-#' @seealso \code{\link{FinancialMarkets}}
+#' library("fmdates")
+#' Currency("AUD", c(AUSYCalendar()))
+#' AUD()
 #' @export
 
-Currency <- R6::R6Class (
-  classname = 'Currency',
-  active = list(
-    iso = function (value) {
-      if (missing(value)) {
-        return(private$.iso)
-      } else {
-        assertthat::assert_that(all(is.character(value)),
-          all(nchar(value) == 3),
-          all(length(value) == length(private$.calendar)))
-        private$.iso <- toupper(value)
-      }
-    },
-    calendar = function (value) {
-      if (missing(value)) {
-        return(private$.calendar)
-      } else {
-        assertthat::assert_that(is(value, "JointCalendar"),
-          all(length(private$.iso) == length(value)))
-        private$.calendar <- value
-      }
-    }
-  ),
-  public = list(
-    initialize = function (iso, calendar) {
-      assertthat::assert_that(
-        assertthat::is.string(iso), nchar(iso) == 3,
-        is(calendar, "JointCalendar"))
-      private$.iso <- toupper(iso)
-      private$.calendar <- calendar
-    },
-    as.character = function () {
-      private$.iso
-    },
-    print = function () {
-      cat("<Currency>", self$as.character(), "\n")
-    }
-  ),
-  private = list(
-    .iso = NA,
-    .calendar = NA)
-)
-
-rep_currency <- function (ccy, times, deep = FALSE) {
-  ccys <- vector("list", times)
-  for (i in seq_along(ccys)) {
-    ccys[[i]] <- ccy$clone(deep)
-  }
-  ccys
+Currency <- function(iso, calendar) {
+  assertthat::assert_that(assertthat::is.string(iso), nchar(iso) == 3,
+    is.JointCalendar(calendar))
+  structure(list(iso = toupper(iso), calendar = calendar), class = "Currency")
 }
+
+#' @rdname Currency
+#' @export
+AUD <- function() Currency("AUD", c(AUSYCalendar()))
+#' @rdname Currency
+#' @export
+EUR <- function() Currency("EUR", c(EUTACalendar()))
+#' @rdname Currency
+#' @export
+GBP <- function() Currency("GBP", c(GBLOCalendar()))
+#' @rdname Currency
+#' @export
+JPY <- function() Currency("JPY", c(JPTOCalendar()))
+#' @rdname Currency
+#' @export
+NZD <- function() Currency("NZD", c(NZAUCalendar(), NZWECalendar()))
+#' @rdname Currency
+#' @export
+USD <- function() Currency("USD", c(USNYCalendar()))
+#' @rdname Currency
+#' @export
+CHF <- function() Currency("CHF", c(CHZHCalendar()))
+#' @rdname Currency
+#' @export
+HKD <- function() Currency("HKD", c(HKHKCalendar()))
+#' @rdname Currency
+#' @export
+NOK <- function() Currency("NOK", c(NOOSCalendar()))
+
+
+#' ISO
+#'
+#' Get ISO from an object. The default method assumes the ISO can be accessed
+#' as if it were an attribute with name `iso` (e.g. `x$iso`).
+#'
+#' @param x object from which to extract an ISO
+#' @return a string of the ISO
+#' @examples
+#' iso(AUD())
+#' @export
+iso <- function(x, ...) UseMethod("iso")
+#' @export
+iso.default <- function(x, ...) x$iso
+#' @export
+locale.Currency <- function(x) x$calendar
+#' @export
+as.character.Currency <- function(x, ...) iso(x)
+#' @export
+format.Currency <- function(x, ...) {paste("<Currency>", x$iso)}
+#' @export
+print.Currency <- function(x, ...) {cat(format(x), "\n"); invisible(x)}
+
