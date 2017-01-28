9ae59311511df83fd57bccde03e331dc46c26114
Imanuel Costigan <i.costigan@me.com>
2017-01-28 21:09:09 +1100

    Translate index classes and methods from R6

--- R/indices-class.R
+++ R/indices-class.R
@@ -1,303 +1,188 @@
-#' @include date-methods.R
-NULL
 
-#' Class \code{"Index"}
-#'
-#' This class sets the basic structure of all other indices.
-#'
-#' @section Active bindings:
-#'
-#' One active binding: \code{name} which either gets or sets the name of the
-#' index (e.g. \code{"AUDBBSW"})
-#'
-#' @docType class
-#' @format A \code{\link{R6}} generator object
-#' @keywords data
-#' @export
+make_index <- function(type, ...) {
+  assertthat::assert_that(is.character(type))
+  structure(list(...), class = c(paste0(type, "Index"), "Index"))
+}
 
-Index <- R6::R6Class (
-  classname = "Index",
-  private = list(
-    .name = NA
-  ),
-  active = list(
-    name = function (value) {
-      if (missing(value)) {
-        private$.name
-      } else {
-        private$.name <- value
-      }
-    }
-  )
-)
 
-#' Class \code{"InterestRateIndex"}
-#'
-#' This can be used to describe any type of interest rate index. It can be used
-#' to describe Ibor indices, or constant maturity swap indices and extends the
-#' \code{\link{Index}} class.
-#'
-#' @section Active bindings:
-#' \describe{
-#' \item{currency}{Gets currency. Setter is unsupported.}
-#' \item{calendar}{Gets calendar. Setter is unsupported.}
-#' \item{day_basis}{Gets day basis. Setter is unsupported.}
-#' \item{pfc_calendar}{Gets currency's calendar. Setter is unsupported.}
-#' \item{spot_lag}{Gets spot lag. Setter is unsupported.}
-#' \item{tenor}{Gets tenor. Setter requires a \code{Period} object.}
-#' }
-#'
-#' @section Inherited bindings:
-#'
-#' One active binding: \code{name} which either gets or sets the name of the
-#' index (e.g. \code{"AUDBBSW"})
-#'
-#' @docType class
-#' @format A \code{\link{R6}} generator object
-#' @keywords data
-#' @export
-
-InterestRateIndex <- R6::R6Class (
-  classname = "InterestRateIndex",
-  inherit = Index,
-  active = list(
-    currency = function (value) {
-      if (missing(value)) {
-        return(private$.currency)
-      } else {
-        stop("Cannot set currency member.")
-      }
-    },
-    calendar = function (value) {
-      if (missing(value)) {
-        return(private$.calendar)
-      } else {
-        stop("Cannot set calendar member.")
-      }
-    },
-    day_basis = function (value) {
-      if (missing(value)) {
-        return(private$.day_basis)
-      } else {
-        stop("Cannot set day_basis member.")
-      }
-    },
-    pfc_calendar = function (value) {
-      if (missing(value)) {
-        return(private$.currency$calendar)
-      } else {
-        stop("Cannot set the currency's calendar member.")
-      }
-    },
-    spot_lag = function (value) {
-      if (missing(value)) {
-        return(private$.spot_lag)
-      } else {
-        stop("Cannot set the spot_lag member.")
-      }
-    },
-    tenor = function (value) {
-      if (missing(value)) {
-        return(private$.tenor)
-      } else {
-        assertthat::assert_that(is(value, "Period"))
-        private$.tenor <- value
-      }
-    }
-  ),
-  private = list(
-    .currency = NA,
-    .tenor = NA,
-    .spot_lag = NA,
-    .calendar = NA,
-    .day_basis = NA)
-)
-
-#' Class \code{"IborIndex"}
+#' IborIndex class
 #'
 #' This can be used to represent IBOR like indices (e.g. LIBOR, BBSW, CDOR)
-#' and extends the \code{InterestRateIndex} class.
+#' and extends the `Index` class.
 #'
-#' @section Public members:
-#' \describe{
-#' \item{initialize(name, currency, tenor, spot_lag, calendar, day_basis,
-#' day_convention, is_eom)}{Creating a new IBOR index. \code{tenor} must be a
-#' \code{Period}}
-#' \item{reset_dates(dates)}{Determines the reset date out of the \code{dates}
-#' by shifting the \code{dates} back by index's spot lag.}
-#' \item{value_dates(dates)}{Determines the value date out of the \code{dates}
-#' by shifting the \code{dates} by the index's spot lag.}
-#' \item{maturity_dates(dates)}{Determines the maturity date out of the \code{dates}
-#' by shifting the \code{dates} by the index's tenor. NB: \code{dates} are
-#' assumed to have been shifted by \code{value_dates()}}
-#' \item{as.character()}{Represent an Index as a character vector}
-#' \item{print()}{Prints an IborIndex object to the REPL}
-#' }
-#'
-#' @section Active bindings:
-#' \describe{
-#' \item{currency}{Gets currency. Setter is unsupported.}
-#' \item{calendar}{Gets calendar. Setter is unsupported.}
-#' \item{day_basis}{Gets day basis. Setter is unsupported.}
-#' \item{pfc_calendar}{Gets currency's calendar. Setter is unsupported.}
-#' \item{spot_lag}{Gets spot lag. Setter is unsupported.}
-#' \item{tenor}{Gets tenor. Setter requires a \code{Period} object.}
-#' \item{day_convention}{Gets day convention. Setter unsupported.}
-#' \item{is_eom}{Gets EOM convention. Setter unsupported.}
-#' }
-#'
-#' @section Inherited bindings:
-#'
-#' One active binding: \code{name} which either gets or sets the name of the
-#' index (e.g. \code{"AUDBBSW"})
-#'
-#' @docType class
-#' @format A \code{\link{R6}} generator object
-#' @keywords data
+#' @param name the name of the index as a string
+#' @param currency the currency associated with the index as a
+#' [Currency][Currency()] object
+#' @param tenor the term of the index as a [period][lubridate::period]
+#' @param spot_lag the period between the index's fixing and the start of
+#' the index's term
+#' @param calendar the calendar used to determine whether the index fixes on a
+#' given date as a [Calendar][fmdates::Calendar]
+#' @param day_basis the day basis associated with the index (e.g. "act/365")
+#' @param day_convention the day convention associated with the index (e.g. "mf")
+#' @param is_eom a flag indicating whether or not the maturity date of the index
+#' is subject to the end-to-end convention.
+#' @return an object of class `IborIndex` that inherits from `Index`
+#' @examples
+#' library(lubridate)
+#' library(fmdates)
+#' # 3m AUD BBSW
+#' IborIndex("BBSW", AUD(), months(3), days(0), c(AUSYCalendar()),
+#'   "act/365", "ms", FALSE)
 #' @export
 
-IborIndex <- R6::R6Class (
-  classname = "IborIndex",
-  inherit = InterestRateIndex,
-  active = list(
-    day_convention = function (value) {
-      if (missing(value)) {
-        return(private$.day_convention)
-      } else {
-        stop("Cannot set the day_convention member.")
-      }
-    },
-    is_eom = function (value) {
-      if (missing(value)) {
-        return(private$.is_eom)
-      } else {
-        stop("Cannot set the is_eom member.")
-      }
-    }
-  ),
-  public = list(
-    initialize = function (name, currency, tenor, spot_lag, calendar,
-      day_basis, day_convention, is_eom) {
-      assertthat::assert_that(is(tenor, "Period"))
-      private$.name <- name
-      private$.currency <- currency
-      private$.tenor <- tenor
-      private$.spot_lag <- spot_lag
-      private$.calendar <- calendar
-      private$.day_basis <- day_basis
-      private$.day_convention <- day_convention
-      private$.is_eom <- is_eom
-      invisible(self)
-    },
-    reset_dates = function (dates) {
-      self$calendar$shift(dates, -private$.spot_lag,
-        private$.day_convention, private$.is_eom)
-    },
-    value_dates = function (dates) {
-      self$calendar$shift(dates, private$.spot_lag,
-        private$.day_convention, private$.is_eom)
-    },
-    maturity_dates = function (dates) {
-      self$pfc_calendar$shift(dates, private$.tenor,
-        private$.day_convention, private$.is_eom)
-    },
-    as.character = function () {
-      paste("<IborIndex>", as.character(private$.tenor), private$.name)
-    },
-    print = function () {
-      cat(self$as.character(), "\n")
-    }
-  ),
-  private = list(
-    .day_convention = NA,
-    .is_eom = NA)
-)
+IborIndex <- function(name, currency, tenor, spot_lag, calendar, day_basis,
+  day_convention, is_eom) {
 
-#' Class \code{"CashIndex"}
+  assertthat::assert_that(
+    assertthat::is.string(name),
+    is.Currency(currency),
+    lubridate::is.period(tenor),
+    lubridate::is.period(spot_lag),
+    fmdates::is.JointCalendar(calendar),
+    fmdates:::is_valid_day_basis(day_basis),
+    fmdates:::is_valid_bdc(day_convention),
+    assertthat::is.flag(is_eom)
+  )
+
+  make_index("Ibor", name = name, currency = currency, tenor = tenor,
+    spot_lag = spot_lag, calendar = calendar, pfc_calendar = locale(calendar),
+    day_basis = day_basis, day_convention = day_convention, is_eom = is_eom
+  )
+
+}
+
+
+#' CashIndex class
 #'
 #' This can be used to represent ONIA like indices (e.g. AONIA, FedFunds)
 #' and extends the \code{InterestRateIndex} class.
 #'
-#' @section Public members:
-#' \describe{
-#' \item{initialize(name, currency, spot_lag, calendar, day_basis,
-#' day_convention)}{Creating a new cash index. The \code{tenor} is set to
-#' one day.}
-#' \item{reset_dates(dates)}{Determines the reset date out of the \code{dates}
-#' by shifting the \code{dates} back by index's spot lag.}
-#' \item{value_dates(dates)}{Determines the value date out of the \code{dates}
-#' by shifting the \code{dates} by the index's spot lag.}
-#' \item{maturity_dates(dates)}{Determines the maturity date out of the \code{dates}
-#' by shifting the \code{dates} by the index's tenor. NB: \code{dates} are
-#' assumed to have been shifted by \code{value_dates()}}
-#' \item{as.character()}{Represent an Index as a character vector}
-#' \item{print()}{Prints a CashIndex object to the REPL}
-#' }
+#' @inheritParams IborIndex
 #'
-#' @section Active bindings:
-#' \describe{
-#' \item{currency}{Gets currency. Setter is unsupported.}
-#' \item{calendar}{Gets calendar. Setter is unsupported.}
-#' \item{day_basis}{Gets day basis. Setter is unsupported.}
-#' \item{pfc_calendar}{Gets currency's calendar. Setter is unsupported.}
-#' \item{spot_lag}{Gets spot lag. Setter is unsupported.}
-#' \item{tenor}{Gets tenor. Setter requires a \code{Period} object.}
-#' \item{day_convention}{Gets day convention. Setter unsupported.}
-#' }
-#'
-#' @section Inherited bindings:
-#'
-#' One active binding: \code{name} which either gets or sets the name of the
-#' index (e.g. \code{"AUDBBSW"})
-#'
-#' @docType class
-#' @format A \code{\link{R6}} generator object
-#' @keywords data
+#' @return an object of class `CashIndex` that inherits from `Index`
+#' @examples
+#' library(lubridate)
+#' library(fmdates)
+#' # RBA cash overnight rate
+#' CashIndex("AONIA", AUD(), days(0), c(AUSYCalendar()), "act/365", "f")
 #' @export
 
-CashIndex <- R6::R6Class (
-  classname = 'CashIndex',
-  inherit = InterestRateIndex,
-  active = list(
-    day_convention = function (value) {
-      if (missing(value)) {
-        return(private$.day_convention)
-      } else {
-        stop("Cannot set the day_convention member.")
-      }
-    }
-  ),
-  public = list(
-    initialize = function (name, currency, spot_lag, calendar,
-      day_basis, day_convention) {
-      private$.name <- name
-      private$.currency <- currency
-      private$.tenor <- lubridate::days(1)
-      private$.spot_lag <- spot_lag
-      private$.calendar <- calendar
-      private$.day_basis <- day_basis
-      private$.day_convention <- day_convention
-      invisible(self)
-    },
-    reset_dates = function (dates) {
-      self$calendar$shift(dates, -private$.spot_lag,
-        private$.day_convention, private$.is_eom)
-    },
-    value_dates = function (dates) {
-      self$calendar$shift(dates, private$.spot_lag,
-        private$.day_convention, FALSE)
-    },
-    maturity_dates = function (dates) {
-      self$pfc_calendar$shift(dates, private$.tenor,
-        private$.day_convention, FALSE)
-    },
-    as.character = function () {
-      private$.name
-    },
-    print = function () {
-      cat("<CashIndex>", self$as.character(), "\n")
-    }
-  ),
-  private = list(
-    .day_convention = NA)
-)
+CashIndex <- function(name, currency, spot_lag, calendar, day_basis,
+  day_convention) {
+
+  assertthat::assert_that(
+    assertthat::is.string(name),
+    is.Currency(currency),
+    lubridate::is.period(spot_lag),
+    fmdates::is.JointCalendar(calendar),
+    fmdates:::is_valid_day_basis(day_basis),
+    fmdates:::is_valid_bdc(day_convention)
+  )
+
+  make_index("Cash", name = name, currency = currency,
+    tenor = lubridate::days(1), spot_lag = spot_lag, calendar = calendar,
+    pfc_calendar = locale(calendar), day_basis = day_basis,
+    day_convention = day_convention, is_eom = FALSE
+  )
+
+}
+
+#' Index date shifters
+#'
+#' A collection of methods that shift dates according to index conventions.
+#'
+#' The following describes the default methods. `to_reset()` treats the input
+#' dates as value dates and shifts these to the corresponding reset or fixing
+#' dates using the index's spot lag; `to_value()` treats the input dates as
+#' reset or fixing dates and shifts them to the corresponding value dates using
+#' the index's spot lag; and `to_maturity()` treats the input dates as value
+#' dates and shifts these to the index's corresponding maturity date using the
+#' index's tenor.
+#'
+#' @param dates a vector of dates to shift
+#' @param index an instance of an object that inherits from the `Index` class.
+#' @return a vector of shifted dates
+#' @examples
+#' library(lubridate)
+#' to_reset(ymd(20170101) + days(0:30), AUDBBSW())
+#' to_value(ymd(20170101) + days(0:30), AUDBBSW())
+#' to_maturity(ymd(20170101) + days(0:30), AUDBBSW())
+#' @name indexshifters
+#' @export
+to_reset <- function(dates, index) UseMethod("to_reset", index)
+#' @rdname indexshifters
+#' @export
+to_value <- function(dates, index) UseMethod("to_value", index)
+#' @rdname indexshifters
+#' @export
+to_maturity <- function(dates, index) UseMethod("to_maturity", index)
+#' @rdname indexshifters
+#' @export
+to_reset.default <- function(dates, index) {
+  fmdates::shift(dates, -index$spot_lag,
+    index$day_convention, index$calendar, index$is_eom)
+}
+#' @rdname indexshifters
+#' @export
+to_value.default <- function(dates, index) {
+  fmdates::shift(dates, index$spot_lag,
+    index$day_convention, index$calendar, index$is_eom)
+}
+#' @rdname indexshifters
+#' @export
+to_maturity.default <- function(dates, index) {
+  fmdates::shift(dates, index$tenor,
+    index$day_convention, c(index$pfc_calendar, index$calendar), index$is_eom)
+}
+
+#' Index class checkers
+#'
+#' @param x an object
+#' @return `TRUE` if object inherits from tested class
+#' @examples
+#' is.Index(AONIA())
+#' is.CashIndex(AONIA())
+#' is.IborIndex(AONIA())
+#' @name indexcheckers
+#' @export
+is.Index <- function(x) inherits(x, "Index")
+#' @rdname indexcheckers
+#' @export
+is.IborIndex <- function(x) inherits(x, "IborIndex")
+#' @rdname indexcheckers
+#' @export
+is.CashIndex <- function(x) inherits(x, "CashIndex")
+
+#' @export
+iso.IborIndex <- function(x) iso(x$currency)
+#' @export
+iso.CashIndex <- function(x) iso(x$currency)
+#' @export
+format.IborIndex <- function(x, ...) {
+  paste("<IborIndex>", pretty_format_period(x$tenor), iso(x), x$name)
+}
+#' @export
+format.CashIndex <- function(x, ...) {
+  paste("<CashIndex>", x$name)
+}
+#' @export
+print.Index <- function(x, ...) {cat(format(x), "\n"); invisible(x)}
+
+
+pretty_format_period <- function(x) {
+  if (length(x@.Data) == 0)
+    return("Period(0)")
+  show <- vector(mode = "character")
+  na <- is.na(x)
+  show <- paste(x@year, "y ", x@month, "m ", x@day, "d ", x@hour,
+    "H ", x@minute, "M ", x@.Data, "S", sep = "")
+  rx <- paste0("((", paste0(0, c("y", "m", "d", "H", "M", "S"), collapse = "|"),
+    ")\\s*)")
+  show <- gsub(rx, "", show)
+  show <- trimws(show, "right")
+  start <- regexpr("[-1-9]|(0\\.)", show)
+  show <- ifelse(start > 0, substr(show, start, nchar(show)), "0S")
+  show[na] <- NA
+  show
+}
