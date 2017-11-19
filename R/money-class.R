9c310b5ff0b70b27c041808be377ac4f57b925fd
Imanuel Costigan <i.costigan@me.com>
2017-11-19 18:03:41 +1100

    First cut of single and multi currency money

--- R/money-class.R
+++ R/money-class.R
@@ -1,11 +1,9 @@
 
-
-new_Money <- function(value, currency) {
-  structure(value, currency = currency, class = "Money")
+new_SingleCurrencyMoney <- function(value, currency) {
+  structure(value, currency = currency, class = "SingleCurrencyMoney")
 }
 
-
-validate_Money <- function(x) {
+validate_SingleCurrencyMoney <- function(x) {
   assertthat::assert_that(
     is.numeric(x),
     is.Currency(attr(x, "currency"))
@@ -13,20 +11,46 @@ validate_Money <- function(x) {
   x
 }
 
-
-Money <- function(value, currency) {
-  validate_Money(new_Money(value, currency))
+SingleCurrencyMoney <- function(value, currency) {
+  validate_SingleCurrencyMoney(new_SingleCurrencyMoney(value, currency))
 }
 
+is.SingleCurrencyMoney <- function(x) {
+  inherits(x, "SingleCurrencyMoney")
+}
 
-
-format.Money <- function(x, ...) {
-  paste("<Money>", attr(x, "currency"),
+format.SingleCurrencyMoney <- function(x, ...) {
+  paste("<SingleCurrencyMoney>", attr(x, "currency"),
     paste0(format(unclass(x)), collapse = " "))
 }
 
+print.SingleCurrencyMoney <- function(x, ...) {
+  cat(format(x), "\n")
+}
 
-#' @export
-print.Money <- function(x, ...) {
+new_MultiCurrencyMoney <- function(...) {
+  structure(list(...), class = "MultiCurrencyMoney")
+}
+
+validate_MultiCurrencyMoney <- function(x) {
+  is_atomic_list(unclass(x), is.SingleCurrencyMoney)
+  x
+}
+
+MultiCurrencyMoney <- function(...) {
+  validate_MultiCurrencyMoney(new_MultiCurrencyMoney(...))
+}
+
+is.MultiCurrencyMoney <- function(x) {
+  inherits(x, "MultiCurrencyMoney")
+}
+
+format.MultiCurrencyMoney <- function(x, ...) {
+  paste0("<MultiCurrencyMoney>\n", paste0("  ",
+    vapply(unclass(x), format, character(1), USE.NAMES = FALSE), collapse = "\n")
+  )
+}
+
+print.MultiCurrencyMoney <- function(x, ...) {
   cat(format(x), "\n")
 }
